0a5fb6fd76cb6ec8672bc5fcbf5b647a246dc5d6
fanialfi <fanialfirdaus@gmail.com>
2023-02-06 19:14:49 +0700

    tipe data string

---
+++ dasar/script/tipe-data-string.js
@@ -0,0 +1,20 @@
+// Tipe data string atau text adalah tipe data yang berisi kumpulan kosong atau lebih karakter
+// bisa dengan menggunakan petik satu (') atau petik dua (")
+
+console.log("Fani Alfirdaus");
+
+// Menambah string
+console.log("Fani " + "Alfirdaus" + " dengan menambahkan string");
+
+// Escape sequence
+/*
+ * \n => enter
+ * \t => tab
+ * \' => '
+ * \" => "
+ * \\ => \
+ */
+
+console.log(
+  'Hello\n \tnama saya adalah\n \t"Fani Alfirdaus"\n \t\\belajar JavaScript dasar'
+);
