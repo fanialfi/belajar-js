68569baf44dae85912e8a23aa948b03dd46c192a
fanialfi <fanialfirdaus@gmail.com>
2023-02-06 19:52:13 +0700

    operasi perbandingan

---
+++ dasar/script/operasi-perbandingan.js
@@ -0,0 +1,20 @@
+/*
+ * Operasi perbandingan adalah operasi untuk membandingkan dua buah data
+ * Operasi perbandingan menghasilkan bilangan boolean
+ *
+ * > => lebih dari
+ * < => kurang dari
+ * >= => lebih dari sama dengan
+ * <= => kurang dari sama dengan
+ * == => sama dengan
+ * === => sama dengan dan sama tipe data
+ * != => tidak sama dengan
+ * !== => tidak sama dengan atau tidak sama dengan type data
+ */
+
+console.log(10 > 1); // true
+console.log(10 >= 10); // true
+console.log(10 == "10"); // true
+console.log(10 === "10"); // false
+console.log(10 != "10"); // false
+console.log(10 !== "10"); // true
