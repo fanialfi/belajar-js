80761202c1732b108805bc69d31349d6278c55c1
fanialfi <fanialfirdaus@gmail.com>
2023-02-06 19:04:22 +0700

    tipe data number

---
+++ dasar/script/tipe-data-number.js
@@ -0,0 +1,10 @@
+// JavaScript hanya mendukung satu tipe data number
+// dimana tipe data di JS bisa berupa bulat atau desimal
+
+console.log(100);
+console.log(100.123);
+
+// JS mendukung number notation, default-nya adalah basis 10
+console.log(0b11110011); // binary
+console.log(0x1ff1); // Hexadecimal
+console.log(0o1100); // Octal
