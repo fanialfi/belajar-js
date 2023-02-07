558bee714c68c14405a1f7d775242ffbe273293c
fanialfi <fanialfirdaus@gmail.com>
2023-02-08 05:09:05 +0700

    console

---
+++ dasar/script/console.js
@@ -0,0 +1,22 @@
+/*
+ * logging adalah mekanisme yang biasa dilakukan oleh programmer untuk menampilkan informasi dari aplikasi yang sedang berjalan,
+ * tanpa harus mengganggu alur kerja aplikasi dan juga interaksi user
+ *
+ * Untuk melakukan ini, kita bisa menggunakan fitur console di JS
+ *
+ * Console Method
+ *
+ * console.debug("hello world")
+ * console.info(memberi informasi)
+ * console.log(memberi informasi)
+ * console.warn(memberi informasi peringatan)
+ * console.error(memberi informasi error)
+ * console.table(memberi informasi dalam bentuk table)
+ */
+
+console.debug("Hello World");
+console.log("Ini informasi");
+console.info("Ini informasi");
+console.warn("Ini informasi Peringatan");
+console.error("Ini informasi error");
+console.table("Ini informasi dalam bentuk table");
