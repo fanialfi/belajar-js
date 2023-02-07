b5ba894e5dd49cd2851693aa8cb77d03cf946d61
fanialfi <fanialfirdaus@gmail.com>
2023-02-08 05:01:54 +0700

    operator logika

---
+++ dasar/script/operator-logika.js
@@ -0,0 +1,19 @@
+/*
+ * && => dan
+ * || => atau
+ * ! => kebalikan
+ */
+console.log(true && true); // true
+console.log(true && false); // false
+console.log(false && true); // false
+console.log(false && false); // false
+
+console.log("");
+console.log(true || true); // true
+console.log(true || false); // true
+console.log(false || true); // true
+console.log(false || false); // false
+
+console.log("");
+console.log(!false); // true
+console.log(!true); // false
