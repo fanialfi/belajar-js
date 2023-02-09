a3846182b83f0834fd170e19671ac43fb9b6dd7b
fanialfi <fanialfirdaus@gmail.com>
2023-02-10 00:35:34 +0700

    popup

---
+++ dasar/script/popup.js
@@ -0,0 +1,8 @@
+// JS memiliki fitur yang bernama alert, prompt, dan confirm
+// alert digunakan untuk memberi peringatan berupa popup text di browser
+// prompt digunakan untuk meminta input string dari pengguna browser dalam bentuk popup input text
+// sedangkan confirm digunakan untuk meminta input boolean dari pengguna browser dalam bentuk popup input pilihan
+
+alert("Hello WOrld");
+prompt("Masukkan data");
+confirm("Sudah Tidur ?");
