e3efea124c547978be39238b44d2fefa2206a81a
fanialfi <fanialfirdaus@gmail.com>
2023-02-10 13:47:36 +0700

    function

---
+++ dasar/script/function.js
@@ -0,0 +1,11 @@
+/* function adalah block kode program yang akan berjalan saat dipanggil
+* funtuk membuat function, kita bisa menggunakan kata kunci function, lalu diikuti dengan nama function
+* kurung() dan diakhiri dengan block kurung{} */
+
+// contoh membuat function
+function sayHello(){
+	console.log("Hello World")
+};
+
+sayHello();
+sayHello(); // boleh dipanggil berkali kali
